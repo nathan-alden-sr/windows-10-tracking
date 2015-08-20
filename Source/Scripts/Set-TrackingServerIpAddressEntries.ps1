415fc1046654ec3ff738a1984566a6e0d1729fd0
Jesse Patching <jesse@patching.ca>
2015-08-19 18:10:21 -0600

    Added IP address blocking

---
+++ Source/Scripts/Set-TrackingServerIpAddressEntries.ps1
@@ -0,0 +1,27 @@
+﻿function Set-TrackingServerIpAddressEntries {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("Add","Remove")]
+        [string] $Action,
+        [string[]] $IpAddresses)
+
+    if (!$IpAddresses) {
+        $IpAddresses = Get-Content (Join-Path $PSScriptRoot "ip-addresses")
+    }
+
+    switch ($Action) {
+        "Add" {
+            Write-Verbose "Blocking tracking server ip addresses in firewall"
+            foreach ($IpAddress in $IpAddresses) {
+                New-NetFirewallRule -DisplayName "Block $IpAddress" -Group "Windows 10 Tracking" -Action block -Direction out -Profile Any -Protocol Any -RemoteAddress $IpAddress
+            }
+        }
+        "Remove" {
+            Write-Verbose "Removing tracking server ip address rules from firewall"
+            foreach ($IpAddress in $IpAddresses) {
+                Remove-NetFirewallRule -DisplayName "Block $IpAddress"
+            }
+        }
+    }
+}
